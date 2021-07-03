406eb2e3fb8dc6274f681a291af88728306c0551
Henni <henniyassine.enpo@gmail.com>
2021-07-03 16:35:59 +0200

    grid list, directives and  cards added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -21,6 +24,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule ,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
