d3d0a98d46a9434e04a8cbaf43c8fe086840bab2
Henni <henniyassine.enpo@gmail.com>
2021-08-17 18:20:31 +0200

    RXJS part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -40,5 +40,9 @@ export class DishService {
    
     return of( DISHES.filter((dish)=> (dish.featured))[0]).pipe(delay(2000));
   }
+
+  getDishIds():Observable<string[] | any >{
+    return of(DISHES.map(dish => dish.id)); //extract the id of each dish and return a new array of ids
+  }
 }
 
